82ca7130ae49404734f1566c1c6e098706594b7f
Diego Pedrosa <linuxmant@gmail.com>
2023-07-12 15:01:15 -0700

    fix some project files

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Libgen_Demo',
+      version='0.1.0',
+      description='',
+      author='Fanzhou Kong',
+      author_email='',
+      packages=[
+		'numpy',
+		'pandas',
+		'seaborn',
+		'tqdm',
+		'ast',
+		'requests',
+		'molmass',
+		'scipy',
+		],
+     )
